346bddc772a29c1bffb7e117e82384b9cc0dc1e2
tdwork-cz <91006489+tdwork-cz@users.noreply.github.com>
2023-09-30 18:30:00 +0200

    Commit from main branch

--- src/main/java/cz/vse/java/xvalm00/adventuracv/main/Adventura.java
+++ src/main/java/cz/vse/java/xvalm00/adventuracv/main/Adventura.java
@@ -12,7 +12,7 @@ public class Adventura {
      * @param args parametry příkazového řádku
      */
     public static void main(String[] args) {
-        IHra hra = new Hra();
+        IHra hra = new Hra();//Comment
         TextoveRozhrani ui = new TextoveRozhrani(hra);
 
         ui.hraj();
